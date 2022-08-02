34f978c7ee0ffca08d77c65a9ace4a28a2869236
Bruno Totoli <santosbruno101062@gmail.com>
2022-08-01 23:26:09 -0300

    Spring Security

---
+++ src/main/java/com/bruno/springmyproject/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.bruno.springmyproject.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("Password encoded {}", encoder.encode("senha"));
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(encoder.encode("senha"))
+                .roles("USER", "ADMIN")
+                .and()
+                .withUser("user")
+                .password(encoder.encode("senhauser"))
+                .roles("USER");
+    }
+}
